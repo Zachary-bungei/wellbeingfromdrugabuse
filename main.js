f5583c5ca54ca7bbd29139b9beea1534d7e2523d
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2024-03-03 09:50:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const searchButton = document.getElementById('searchbutton');
+const searchInput = document.getElementById('searchbox');
+
+searchButton.addEventListener('click', function() {
+const query = searchInput.value; // Get the search query from the input field
+const url = `https://example.com/search?q=${query}`; // Construct the URL for the search API endpoint with the query parameter
+window.location.href = url;
+ter
+});
+
+document.getElementById("printButton").addEventListener("click", function() {
+	window.print();
+});
